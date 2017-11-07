c6fb1751882b573d07492704779f9877fe04b4b0
Alea Nalesnik <aleanalesnik@Aleas-MacBook-Air.local>
2017-11-07 14:17:57 +0100

    new files and folders

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-let container = require('./json-file-reader.js');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
 
-container.filerReader ('countries.json', function(data) {
-    let countries = data;
-    
-    for (var i = 0; i < countries.length; i++) {
-    	var input = process.argv[2]    // input that user gives
-    	if (input === countries[i].name) {
-    		console.log("Country: " + input + countries[i].topLevelDomain);
-    	}
-    }
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('public')); // need this
+
+
+
+//ROUTES
+
+// home page
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
+// search page
+app.get("/search", (req, res) => {
+    res.render("search")
+})
 
+// search results
+app.get("/search_result", function(req, res) {
+    var input = req.query.input;
+    fs.readFile("./countries.json", function(err, data) {
+        if (err) {
+            throw err
+        }
+        var parsedJson = JSON.parse(data);
+        var output = parsedJson.find(element => {
+            return element.name.toLowerCase() === input.toLowerCase();
+        });
+        console.log(output)
+        res.send({ output: output })
+    })
+})
+
+// about page
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
+
+app.listen(3000, function(res) {
+    console.log("App listening on port 3000!");
+})
